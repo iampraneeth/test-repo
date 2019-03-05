5932aad43451b7732d241d976d1b0e0077b0ae8d
Rajput <pratik-gajendrasing.rajput@capgemini.com>
2019-03-05 10:42:33 +0530

    added subtraction

--- Calculator.java
+++ Calculator.java
@@ -7,4 +7,8 @@ public static void addition(int number1, int number 2){
 System.out.println("Addition="+(number1+number2)); 
     
 }
+public static void subtarction(int number1, int number 2){
+System.out.println("subtraction="+(number1-number2)); 
+    
+}
 }
