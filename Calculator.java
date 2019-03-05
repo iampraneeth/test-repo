60c8859f4995ee155af42b39e9510411dd4d845b
Rajput <pratik-gajendrasing.rajput@capgemini.com>
2019-03-05 10:26:56 +0530

    added addition function

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,10 @@
 public class Calculator{
     public static void main(String [] args){
      System.out.println("Hello World!!!");
+}
      System.out.println("Calculator java file");
+public static void addition(int number1, int number 2){
+System.out.println("Addition="+(number1+number2)); 
+    
 }
 }
